f910fc01e93c2d6cc4f81d7bd5abb2863478593b
ShanKonduru <ShanKonduru@gmail.com>
2023-12-14 16:44:34 +0530

    added 9 dots and lines connecting them

--- plotcircle.py
+++ plotcircle.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
-def plot_circle_with_dots(diameter=50, num_dots=9):
+def plot_circle_with_dots_and_lines(diameter=50, num_dots=9):
     radius = diameter / 2
     theta = np.linspace(0, 2 * np.pi, num_dots, endpoint=False)
 
@@ -19,15 +20,23 @@ def plot_circle_with_dots(diameter=50, num_dots=9):
         ax.plot(dot_x, dot_y, marker='o', markersize=8, label=str(i))
         ax.text(dot_x, dot_y, str(i), ha='center', va='center', fontsize=10, fontweight='bold')
 
+    # Lines connecting specific dots with random colors
+    pairs = [(1, 2), (7, 8), (3, 9), (9, 6), (3, 6), (1, 5), (2, 4), (7, 5), (8, 4)]
+    for pair in pairs:
+        color = "#{:06x}".format(random.randint(0, 0xFFFFFF))  # Generate random hex color
+        line = plt.Line2D([x[pair[0] - 1], x[pair[1] - 1]], [y[pair[0] - 1], y[pair[1] - 1]],
+                          color=color, linestyle='-', linewidth=1)
+        ax.add_line(line)
+
     # Set aspect ratio to be equal, and adjust plot limits
     ax.set_aspect('equal', adjustable='box')
     ax.set_xlim([-radius, radius])
     ax.set_ylim([-radius, radius])
 
     # Add title
-    ax.set_title('Circle with Dots on Perimeter')
+    ax.set_title('Circle with Dots and Connecting Lines')
 
     plt.show()
 
 # Example usage with default values
-plot_circle_with_dots()
+plot_circle_with_dots_and_lines()
